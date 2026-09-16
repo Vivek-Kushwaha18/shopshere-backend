a13eeb20d6240469b42e4e70dda7fea8b0c7353c
vivek kushwaha <vivekkushwaha285@gmail.com>
2026-09-16 14:37:55 +0530

    backend main

--- main.py
+++ main.py
@@ -1,33 +1,61 @@
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
-import os 
-# for apis
+# For APIs
 from app.api.auth import router as auth_router
 from app.api.products import router as products_router
-from fastapi.middleware.cors import CORSMiddleware
 
-
-
-
-
-#for database or models
+# For database
 from app.database.database import engine
 
-#for model
+# For models
 from app.models.base import Base
 from app.models.user import User
 from app.models.product import Product
 
-app = FastAPI (
-  title ="ShopShere API",
-)
-# also api part
-app.include_router(auth_router)
 
+app = FastAPI(
+    title="ShopShere API",
+)
+
+
+# =========================
+# CORS
+# =========================
+
+FRONTEND_URL = os.getenv(
+    "FRONTEND_URL",
+    "http://localhost:3000"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        FRONTEND_URL,
+        "https://shopshere-frontend-theta.vercel.app",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# =========================
+# API ROUTES
+# =========================
+
+app.include_router(auth_router)
 app.include_router(products_router)
 
-# data base part
+
+# =========================
+# DATABASE
+# =========================
+
 @app.on_event("startup")
 async def create_tables():
 
@@ -36,19 +64,14 @@ async def create_tables():
         await connection.run_sync(
             Base.metadata.create_all
         )
-# comman
+
+
+# =========================
+# ROOT API
+# =========================
+
 @app.get("/")
 async def root():
-    return {"message": "ShopSphere API is running"}
-
-FRONTEND_URL = os.getenv(
-  "FRONTEND_URL",
-  "http://localhost:3000"
-)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[FRONTEND_URL],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    return {
+        "message": "ShopSphere API is running"
+    }
