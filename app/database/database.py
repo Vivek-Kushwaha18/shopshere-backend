be4e9291108b083d521c78db0065e89d1be1b9c7
vivek kushwaha <vivekkushwaha285@gmail.com>
2026-09-16 12:25:24 +0530

    smt

--- app/database/database.py
+++ app/database/database.py
@@ -1,4 +1,3 @@
-
 import os
 
 from dotenv import load_dotenv
@@ -14,20 +13,27 @@ from sqlalchemy.ext.asyncio import (
 load_dotenv()
 
 
-# Get database URL from .env
+# Get database URL
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 if not DATABASE_URL:
-    raise ValueError(
-        "DATABASE_URL is not set in .env"
-    )
+    raise ValueError("DATABASE_URL is not set")
+
+
+# Remove PostgreSQL parameters that asyncpg does not accept directly
+DATABASE_URL = DATABASE_URL.replace("sslmode=require", "")
+DATABASE_URL = DATABASE_URL.replace("channel_binding=require", "")
+DATABASE_URL = DATABASE_URL.rstrip("?&")
 
 
 # Create database engine
 engine = create_async_engine(
     DATABASE_URL,
-    echo=False
+    connect_args={
+        "ssl": True
+    },
+    echo=False,
 )
 
 
@@ -35,7 +41,7 @@ engine = create_async_engine(
 SessionLocal = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
-    expire_on_commit=False
+    expire_on_commit=False,
 )
 
 
